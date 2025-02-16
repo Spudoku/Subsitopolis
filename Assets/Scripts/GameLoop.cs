f54adc6b020dfea4b35ea5ad100d049e3650a56d
Spudoku <calebisntshort@gmail.com>
2025-02-16 11:44:45 -0800

    inital brainstorming on GameLoop.cs 30 minutes

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -1,16 +1,34 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameLoop : MonoBehaviour
 {
+    const float DEFAULT_TICK_RATE = 0.1429f;        // 1 tick per 7 seconds
+    //const float SLOW_TICK_RATE = 0.0714f;
+    //const float FAST_TICK_RATE = 0.2857f;
+    public float tickrate;     // speed of ticks/second, with a tick being a month in game
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tickrate = DEFAULT_TICK_RATE;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // goal: make ticks occur once every (1/tickrate) * time.timeScale seconds seconds
+        // 
+    }
+
+
+
+    // execute a single tick in the game
+    // a tick represents one game month
+    private void tick()
+    {
+        Debug.Log("Executing a tick at " + Time.time);
     }
 }
