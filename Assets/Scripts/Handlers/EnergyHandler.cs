ca542f7ff8a86e8d6a6927ba29f962b6721a40d8
Spudoku <calebisntshort@gmail.com>
2025-02-17 17:47:54 -0800

    Experimentation on how to determine how much energy can be produced
    30 minutes

--- Assets/Scripts/Handlers/EnergyHandler.cs
+++ Assets/Scripts/Handlers/EnergyHandler.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,12 +9,15 @@ public class EnergyHandler : Handler
 {
     public override void Initialze()
     {
+
+
         // to produce 1 megawatt hour of energy, you need:
         foodFactor = 0f;
         waterFactor = 0f;
         energyFactor = 0f;
 
         maxFunding = 0.5f;           // $0.50 per kilowatt/hour; half a million dollars per month
+
 
     }
 
@@ -21,6 +26,17 @@ public class EnergyHandler : Handler
 
         // produce as much as possible given factors
         // figure out how many megawatt-hours can be produced given the resources
+        // step 1: estimate how many units can be produced by funding/maxFunding
+        float units = funding / maxFunding;
+        HypotheticalUnits();
+
+        float finalUnits = 0;
+        Debug.Log($"[EnergyHandler] food units: {unitsWithFood}, units with Energy: {unitsWithEnergy}, units with Water: {unitsWithWater}, funding units: {units}, finalUnits: {finalUnits}");
+
+        // step 2: determine ratio of production/demand for each resource in Factors
+        // using a hypothetical amount of (currentProduction / currentKnownDemand) * <resource>Factor + hypotheicalDemand
+        // hypotheical demand = units * <resource>Factor
+
 
 
         // example: 100 million a month should yield 200 million megawatt hours a month
